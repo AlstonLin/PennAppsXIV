4b12c9aa89d231459f01eb45d33ab0b183503e5d
daose <bychen@uwaterloo.ca>
2016-09-10 21:39:07 -0400

    player_id fix

--- Assets/Scripts/SpaceShip.cs
+++ Assets/Scripts/SpaceShip.cs
@@ -83,7 +83,7 @@ public class SpaceShip : MonoBehaviour, IGvrGazeResponder {
 		ammoAmount--;
 		//setAmmoText ();
 		JSONObject json = new JSONObject ();
-		json.AddField ("id", id);
+		json.AddField ("player_id", id);
 		socket.Emit ("shot_fired");
     }
 
