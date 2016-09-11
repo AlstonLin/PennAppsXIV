ddaae8f91fce819b9ba1605fc10503bc5dd1e7c2
daose <bychen@uwaterloo.ca>
2016-09-10 22:11:04 -0400

    spaceshipskeletons shouldn't get health deducted

--- Assets/Scripts/SpaceShipSkeleton.cs
+++ Assets/Scripts/SpaceShipSkeleton.cs
@@ -67,7 +67,6 @@ public class SpaceShipSkeleton : MonoBehaviour, IGvrGazeResponder {
 
     void OnCollisionEnter(Collision collisionInfo) {
         Debug.Log("spaceship: onCollisionEnter");
-		hp--;
     }
 
     public void onDeath() {
